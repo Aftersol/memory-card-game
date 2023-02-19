8f4d11266d12b2035f0189699493a2026b348f48
Aftersol <42478428+Aftersol@users.noreply.github.com>
2023-02-18 21:03:13 -0800

    pizza game scrapped

--- app.js
+++ app.js
@@ -1,87 +1,8 @@
-class Position
-{
-    x;
-    y;
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class Player
-{
-    pos = Position(x,y);
-    holding;
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
-
-    move(x, y)
-    {
-        this.pos.x += x;
-        this.pos.y += y;
-    }
-}
-
-class Pizza
-{
-    topping;
-    bakeTime;
-    isBurned;
-
-    burnPizza()
-    {
-        this.isBurned = true;
-    }
-
-    constructor()
-    {
-        this.isBurned = false;
-    }
-}
-
-class Oven
-{
-    pizza;
-
-    set pizza(pizza)
-    {
-        this.pizza = pizza;
-    }
-
-    get pizza()
-    {
-        return this.pizza;
-    }
-}
-
-class Order
-{
-    pizza;
-    drink;
-
-    constructor(pizza, hasDrink)
-    {
-        this.pizza = pizza;
-        this.drink = hasDrink;
-    }
-}
-
 class Game
 {
-    score;
-    timer;
-    orders;
-    ordersFulfilled;
-    maxOrders;
 
     constructor() {
-        this.scrore = 0;
-        this.timer = 0;
-        this.orders = Array<order>[];
-        this.ordersFulfilled = 0
-        this.maxOrders = 5;
+
     }
 }
 
